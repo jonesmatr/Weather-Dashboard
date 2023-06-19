92dc311d7c33cf9afbe0c585ed2d2a3d141c7b3e
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-06-19 08:47:17 -0400

    Use Bootstrap modify the layout of the page. Change the JavaScript to include a search feature and search history.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,36 @@
 const apiKey = '922e43f3a98d8aba52633511ec6358f3'; // Replace with your API key
-const lat = '37.5407'; // Replace with the latitude
-const lon = '77.4360'; // Replace with the longitude
-var city;
+let searchHistory = [];
 
-fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
-  .then(response => response.json())
-  .then(data => {
-    const weatherContainer = document.getElementById('weather-container');
-    data.list.forEach((forecast, index) => {
-      if (index % 8 === 0) { // Only take one forecast per day
-        const forecastElement = document.createElement('div');
-        forecastElement.innerHTML = `
-          <h2>${new Date(forecast.dt_txt).toLocaleDateString()}</h2>
-          <p>Temperature: ${forecast.main.temp}°K</p>
-          <p>Weather: ${forecast.weather[0].description}</p>
-        `;
-        weatherContainer.appendChild(forecastElement);
-      }
+document.getElementById('search-button').addEventListener('click', () => {
+  const city = document.getElementById('search-input').value;
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+      const weatherContainer = document.getElementById('weather-container');
+      weatherContainer.innerHTML = '';
+      data.list.forEach((forecast, index) => {
+        if (index % 8 === 0) { // Only take one forecast per day
+          const forecastElement = document.createElement('div');
+          forecastElement.className = 'card p-3 mb-2';
+          forecastElement.innerHTML = `
+            <h2>${new Date(forecast.dt_txt).toLocaleDateString()}</h2>
+            <p>Temperature: ${forecast.main.temp}°K</p>
+            <p>Weather: ${forecast.weather[0].description}</p>
+          `;
+          weatherContainer.appendChild(forecastElement);
+        }
+      });
+
+      // Update search history
+      searchHistory = [city, ...searchHistory.slice(0, 9)];
+      const searchHistoryElement = document.getElementById('search-history');
+      searchHistoryElement.innerHTML = '';
+      searchHistory.forEach(city => {
+        const li = document.createElement('li');
+        li.className = 'list-group-item';
+        li.textContent = city;
+        searchHistoryElement.appendChild(li);
+      });
     });
-  });
+});
+
