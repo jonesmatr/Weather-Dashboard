04a508e314514e25c7469873da6aa7952640a368
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-06-21 09:17:44 -0400

    Fixed issue with 5-Day Forecast not displaying base on user's geolocation when first opening the page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 const apiKey = '922e43f3a98d8aba52633511ec6358f3'; // API key
 let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || []; // Search history
 var currentWeather = document.getElementById("current-weather");
-//Add more variables here that get elements from the HTML page to use in the functions below
-//Can you use if statements to make the page display the weather for the city the user searches for?
-//Check 22-Stu_Review-Part-One in Module 6 for an example of how to use if statements to display information on the page
+var searchButton = document.getElementById("search-button");
+var searchCity = document.getElementById("search-input");
+var weatherContainer = document.getElementById("weather-container");
+const searchHistoryElement = document.getElementById('search-history');
 
-// Function to fetch weather data based on latitude and longitude
+// Function to fetch weather data and five-day forecast based on latitude and longitude
 async function fetchWeatherByLocation(lat, lon) {
   // Fetch current weather
   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`);
@@ -22,7 +23,30 @@ async function fetchWeatherByLocation(lat, lon) {
     <p>Humidity: ${data.main.humidity}%</p>
     <p>Weather: ${data.weather[0].description}</p>
   `;
-}
+
+  const forecastResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`);
+  const forecastData = await forecastResponse.json();
+       
+    weatherContainer.innerHTML = '';
+    forecastData.list.forEach((forecast, index) => {
+      if (index % 8 === 0) { // Only take one forecast per day
+        const forecastElement = document.createElement('div');
+        forecastElement.className = 'card p-3 mb-2';
+        forecastElement.innerHTML = `
+          <h2>${new Date(forecast.dt_txt).toLocaleDateString()}</h2>
+          <p><img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png"/></p>
+          <p>Temperature: ${forecast.main.temp}°F</p>
+          <p>Wind Speed: ${forecast.wind.speed} mph</p>
+          <p>Humidity: ${forecast.main.humidity}%</p>
+          <p>Weather: ${forecast.weather[0].description}</p>
+        `;
+        weatherContainer.appendChild(forecastElement);
+      }
+
+    }); 
+  };
+
+
 
 window.addEventListener('load', () => {
   if (navigator.geolocation) {
@@ -37,47 +61,15 @@ window.addEventListener('load', () => {
     // Geolocation is not supported by this browser
     console.log('Geolocation is not supported by this browser.');
   }
-
-// Function to fetch weather data based on latitude and longitude
-async function fetchWeatherByLocation(lat, lon) {
-    // Fetch current weather
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`);
-    const data = await response.json();
-  
-     // Display the weather data
-  const date = new Date();
-  const dateString = date.toLocaleDateString();
-  const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`; // Construct icon URL
-  currentWeather.innerHTML = `
-    <h2>${data.name} - (${dateString}) <img src="${iconUrl}" alt="Weather icon"></h2>
-    <p>Temperature: ${data.main.temp}°F</p>
-    <p>Wind Speed: ${data.wind.speed} mph</p>
-    <p>Humidity: ${data.main.humidity}%</p>
-    <p>Weather: ${data.weather[0].description}</p>
-  `;
-}
-
-  
-  window.addEventListener('load', () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(position => {
-        const lat = position.coords.latitude;
-        const lon = position.coords.longitude;
-  
-        // Fetch weather data for the user's current location
-        fetchWeatherByLocation(lat, lon);
-      });
-    } else {
-      // Geolocation is not supported by this browser
-      console.log('Geolocation is not supported by this browser.');
-    }
-    });
   
  
 document.getElementById('search-button').addEventListener('click', () => {
     const city = document.getElementById('search-input').value;
   
 //I want this information to be displayed on the page when the user searches for a city, then added to local storage
+
+
+
 
     // Fetch current weather
 function fetchWeather(city) {
@@ -97,17 +89,22 @@ function fetchWeather(city) {
     localStorage.setItem('weatherData', JSON.stringify(data));
   });
 }
+
+
+
 // Remove city from search history if it's already there
   const index = searchHistory.indexOf(city);
   if (index > -1) {
     searchHistory.splice(index, 1);
   }
 
+
+
+
   // Fetch five-day forecast
   fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`)
     .then(response => response.json())
-    .then(data => {
-      const weatherContainer = document.getElementById('weather-container');
+    .then(data => {      
       weatherContainer.innerHTML = '';
       data.list.forEach((forecast, index) => {
         if (index % 8 === 0) { // Only take one forecast per day
@@ -125,9 +122,13 @@ function fetchWeather(city) {
         }
       });     
 
+
+
+
+
+      
       // Update search history
       searchHistory = [city, ...searchHistory.slice(0, 9)];
-      const searchHistoryElement = document.getElementById('search-history');
       searchHistoryElement.innerHTML = '';
       searchHistory.forEach(city => {
         const li = document.createElement('li');
